fa78d76e4ff5664b6444bfcf4a4fa544ca98c26a
Centeno Jose Antonio <jocecito182@gmail.com>
2024-06-14 14:03:20 -0300

    Add webhook debug

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -3,39 +3,38 @@ import { headers } from 'next/headers'
 import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { NextResponse } from 'next/server'
- 
+
 export async function POST(req: Request) {
- 
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
- 
+
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
- 
-  // Get the headers
+
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
- 
-  // If there are no headers, error out
+
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400
     })
   }
- 
-  // Get the body
+
   const payload = await req.json()
   const body = JSON.stringify(payload);
- 
-  // Create a new Svix instance with your secret.
+
+  // Depuración: imprime los valores recibidos
+  console.log("svix-id:", svix_id);
+  console.log("svix-timestamp:", svix_timestamp);
+  console.log("svix-signature:", svix_signature);
+  console.log("Payload body:", body);
+
   const wh = new Webhook(WEBHOOK_SECRET);
- 
+
   let evt: WebhookEvent
- 
-  // Verify the payload with the headers
+
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -44,15 +43,14 @@ export async function POST(req: Request) {
     }) as WebhookEvent
   } catch (err) {
     console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
+    return new Response('Error occurred', {
       status: 400
     })
   }
- 
-  // Get the ID and type
+
   const { id } = evt.data;
   const eventType = evt.type;
- 
+
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
@@ -100,7 +98,6 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ message: 'OK', user: deletedUser })
   }
- 
+
   return new Response('', { status: 200 })
 }
- 
